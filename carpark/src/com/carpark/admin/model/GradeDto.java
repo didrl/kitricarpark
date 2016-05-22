5bf890230666530f6d37ce1eca1777c73c02f2bc
이현택 <zms0529@gmail.com>
2016-05-23 01:58:15 +0900

    Dto 생성

--- carpark/src/com/carpark/admin/model/GradeDto.java
+++ carpark/src/com/carpark/admin/model/GradeDto.java
@@ -1,5 +1,41 @@
 package com.carpark.admin.model;
 
 public class GradeDto {
+	private int grade_id;
+	private String grade_name;
+	private int avgpoint;
+	private int benefit;
+	private int penalty;
+	
+	public int getGrade_id() {
+		return grade_id;
+	}
+	public void setGrade_id(int grade_id) {
+		this.grade_id = grade_id;
+	}
+	public String getGrade_name() {
+		return grade_name;
+	}
+	public void setGrade_name(String grade_name) {
+		this.grade_name = grade_name;
+	}
+	public int getAvgpoint() {
+		return avgpoint;
+	}
+	public void setAvgpoint(int avgpoint) {
+		this.avgpoint = avgpoint;
+	}
+	public int getBenefit() {
+		return benefit;
+	}
+	public void setBenefit(int benefit) {
+		this.benefit = benefit;
+	}
+	public int getPenalty() {
+		return penalty;
+	}
+	public void setPenalty(int penalty) {
+		this.penalty = penalty;
+	}
 	
 }
--- carpark/src/com/carpark/admin/model/ParkingDto.java
+++ carpark/src/com/carpark/admin/model/ParkingDto.java
@@ -1,5 +1,62 @@
 package com.carpark.admin.model;
 
 public class ParkingDto {
+	private int park_id;
+	private String park_name;
+	private String location;
+	private int park_capacity;
+	private String owner_id;
+	private int latitude;
+	private int longitude;
+	private String park_type;
+	public int getPark_id() {
+		return park_id;
+	}
+	public void setPark_id(int park_id) {
+		this.park_id = park_id;
+	}
+	public String getPark_name() {
+		return park_name;
+	}
+	public void setPark_name(String park_name) {
+		this.park_name = park_name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getPark_capacity() {
+		return park_capacity;
+	}
+	public void setPark_capacity(int park_capacity) {
+		this.park_capacity = park_capacity;
+	}
+	public String getOwner_id() {
+		return owner_id;
+	}
+	public void setOwner_id(String owner_id) {
+		this.owner_id = owner_id;
+	}
+	public int getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	public String getPark_type() {
+		return park_type;
+	}
+	public void setPark_type(String park_type) {
+		this.park_type = park_type;
+	}
+	
 	
 }
