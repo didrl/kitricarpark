eb9b4273da2b1d31cf967faa75abf1af9003b253
didrl <didrl3382@gmail.com>
2016-05-29 05:42:20 +0900

    주석추가

--- carpark/src/com/carpark/member/model/service/MemberMessageServiceImpl.java
+++ carpark/src/com/carpark/member/model/service/MemberMessageServiceImpl.java
@@ -21,28 +21,28 @@ public class MemberMessageServiceImpl implements MemberMessageService {
 	}
 
 	@Override
-	public int writeArticle(MessageDto messageDto) {
+	public int writeArticle(MessageDto messageDto) {//쪽지보내기
 		return MemberMessageDaoImpl.getMemberMessageDao().writeArticle(messageDto);
 	}
 
 	@Override
-	public MessageDto viewArticle(int seq) {
+	public MessageDto viewArticle(int seq) {//쪽지보기
 		return MemberMessageDaoImpl.getMemberMessageDao().viewArticle(seq);
 	}
 
 	@Override
-	public void deleteArticle(int seq) {
+	public void deleteArticle(int seq) {//쪽지삭제
 		MemberMessageDaoImpl.getMemberMessageDao().deleteArticle(seq);
 		
 	}
 
 	@Override
-	public List<MessageDto> sendListArticle(String userId) {
+	public List<MessageDto> sendListArticle(String userId) {//보낸쪽지리스트
 		return MemberMessageDaoImpl.getMemberMessageDao().sendListArticle(userId);
 	}
 
 	@Override
-	public List<MessageDto> receiveListArticle(String receiveId) {
+	public List<MessageDto> receiveListArticle(String receiveId) {//받은쪽지리스트
 		return MemberMessageDaoImpl.getMemberMessageDao().receiveListArticle(receiveId);
 	}
 	
