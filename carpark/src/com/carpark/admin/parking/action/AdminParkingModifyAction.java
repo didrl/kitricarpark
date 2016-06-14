d4a4f8d19bd47b91804a7e3fbfcafce3aafc6c77
zms0529 <zms0529@google.com>
2016-06-14 18:53:28 +0900

    주소치고 못들어가게

--- carpark/src/com/carpark/admin/parking/action/AdminParkingModifyAction.java
+++ carpark/src/com/carpark/admin/parking/action/AdminParkingModifyAction.java
@@ -24,55 +24,62 @@ public class AdminParkingModifyAction implements Action {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		
+
 		int pg = NumberCheck.nullToOne(request.getParameter("pg"));
 		String key = StringCheck.nullToBlank(request.getParameter("key"));
 		String word = StringCheck.nullToBlank(Encoder.isoToUtf(request.getParameter("word")));
-		
+
 		HttpSession session = request.getSession();
 		MemberDto memberDto = (MemberDto) session.getAttribute("memberInfo");
-		String ownerId = memberDto.getUser_id();
-		
-		ParkingDetailDto parkingDto = new ParkingDetailDto();
-		
-		parkingDto.setPark_id(NumberCheck.nullToZero(request.getParameter("parkId")));
-		parkingDto.setPark_type(request.getParameter("parkType"));
-		parkingDto.setPark_name(request.getParameter("parkName"));
-		
-		String coordinate = request.getParameter("coordinate");//지도에서 가져온 좌표
-		if(coordinate != null) {
-		StringTokenizer st = new StringTokenizer(coordinate, ",");//lat, lng로 나눔
-		String latitude = st.nextToken().substring(1);// ( 제거 
-		String longitude = st.nextToken().replace(")", "").trim();// ) 제거
-		
-		parkingDto.setLatitude(Double.parseDouble(latitude));//dto에 넣기
-		parkingDto.setLongitude(Double.parseDouble(longitude));
-		} else {
-			System.out.println("좌표가져오기 실패");
-			return "/parking/register.jsp";
-		}
-		
-		parkingDto.setOwner_id(ownerId);
-		
-		parkingDto.setPay_yn(request.getParameter("payYn"));
-		parkingDto.setSatur_pay_yn(request.getParameter("saturPayYn"));
-		parkingDto.setHoli_pay_yn(request.getParameter("holiPayYn"));
-		parkingDto.setPark_capacity(NumberCheck.nullToOne(request.getParameter("parkCapacity")));
-		parkingDto.setPark_time_rate(NumberCheck.nullToOne(request.getParameter("parkTimeRate")));
-		parkingDto.setPark_rate(NumberCheck.nullToOne(request.getParameter("parkRate")));
-		parkingDto.setAdd_park_rate(NumberCheck.nullToOne(request.getParameter("addParkRate")));
-		parkingDto.setDay_max_pay(NumberCheck.nullToOne(request.getParameter("dayMaxPay")));
-		parkingDto.setFulltime_monthly_pay(NumberCheck.nullToOne(request.getParameter("fullTimeMonthlyPay")));
-		parkingDto.setPark_flag(NumberCheck.nullToOne(request.getParameter("parkFlag")));
-		parkingDto.setContent(request.getParameter("content").replace("\r\n", "<br>"));
-		parkingDto.setDetailAddr(request.getParameter("parkAddress"));
-		
-		parkingDto.setEmd_code(11650101);
-		
-		MemberParkingServiceImpl.getMemberParkingservice().parkingModify(parkingDto);
-		
-		
-		return "/admin/parking/list.jsp";
+
+		if (memberDto != null) {
+			String ownerId = memberDto.getUser_id();
+
+			ParkingDetailDto parkingDto = new ParkingDetailDto();
+
+			parkingDto.setPark_id(NumberCheck.nullToZero(request.getParameter("parkId")));
+			parkingDto.setPark_type(request.getParameter("parkType"));
+			parkingDto.setPark_name(request.getParameter("parkName"));
+
+			String coordinate = request.getParameter("coordinate");// 지도에서 가져온
+																	// 좌표
+			if (coordinate != null) {
+				StringTokenizer st = new StringTokenizer(coordinate, ",");// lat,
+																			// lng로
+																			// 나눔
+				String latitude = st.nextToken().substring(1);// ( 제거
+				String longitude = st.nextToken().replace(")", "").trim();// )
+																			// 제거
+
+				parkingDto.setLatitude(Double.parseDouble(latitude));// dto에 넣기
+				parkingDto.setLongitude(Double.parseDouble(longitude));
+			} else {
+				System.out.println("좌표가져오기 실패");
+				return "/parking/register.jsp";
+			}
+
+			parkingDto.setOwner_id(ownerId);
+
+			parkingDto.setPay_yn(request.getParameter("payYn"));
+			parkingDto.setSatur_pay_yn(request.getParameter("saturPayYn"));
+			parkingDto.setHoli_pay_yn(request.getParameter("holiPayYn"));
+			parkingDto.setPark_capacity(NumberCheck.nullToOne(request.getParameter("parkCapacity")));
+			parkingDto.setPark_time_rate(NumberCheck.nullToOne(request.getParameter("parkTimeRate")));
+			parkingDto.setPark_rate(NumberCheck.nullToOne(request.getParameter("parkRate")));
+			parkingDto.setAdd_park_rate(NumberCheck.nullToOne(request.getParameter("addParkRate")));
+			parkingDto.setDay_max_pay(NumberCheck.nullToOne(request.getParameter("dayMaxPay")));
+			parkingDto.setFulltime_monthly_pay(NumberCheck.nullToOne(request.getParameter("fullTimeMonthlyPay")));
+			parkingDto.setPark_flag(NumberCheck.nullToOne(request.getParameter("parkFlag")));
+			parkingDto.setContent(request.getParameter("content").replace("\r\n", "<br>"));
+			parkingDto.setDetailAddr(request.getParameter("parkAddress"));
+
+			// parkingDto.setEmd_code(11650101);
+
+			MemberParkingServiceImpl.getMemberParkingservice().parkingModify(parkingDto);
+
+			return "/admin/parking/list.jsp";
+		} else
+			return "";
 	}
 
 }
